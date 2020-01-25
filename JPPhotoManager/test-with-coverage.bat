8e44137ef559cbc824b27dc7d9d86e30d66bfc13
Juan Pablo Drexler <jpablodrexler@outlook.com>
2020-01-24 23:28:31 -0300

    Fixes parameter in bat file.

--- JPPhotoManager/test-with-coverage.bat
+++ JPPhotoManager/test-with-coverage.bat
@@ -1,4 +1,4 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*Test*]" /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromTestsCodeCoverageAttribute"
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*Tests*]" /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromTestsCodeCoverageAttribute"
 
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
 .\tools\reportgenerator.exe -reports:JPPhotoManager.Tests\TestResults\coverage.info -targetdir:.\TestResults\
