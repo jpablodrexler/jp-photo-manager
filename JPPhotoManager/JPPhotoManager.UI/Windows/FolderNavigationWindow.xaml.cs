5b1a36b927d2fbe327039ebcc5adbce749119787
Juan Pablo Drexler <jpablodrexler@outlook.com>
2020-09-05 13:58:11 -0300

    Adds key shortcuts for confirm and cancel.

--- JPPhotoManager/JPPhotoManager.UI/Windows/FolderNavigationWindow.xaml.cs
+++ JPPhotoManager/JPPhotoManager.UI/Windows/FolderNavigationWindow.xaml.cs
@@ -4,6 +4,7 @@ using log4net;
 using System;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Input;
 
 namespace JPPhotoManager.UI.Windows
 {
@@ -43,13 +44,37 @@ namespace JPPhotoManager.UI.Windows
 
         private void Confirm_MouseLeftButtonDown(object sender, RoutedEventArgs e)
         {
+            Confirm();
+        }
+
+        private void Cancel_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            Cancel();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    Confirm();
+                    break;
+
+                case Key.Escape:
+                    Cancel();
+                    break;
+            }
+        }
+
+        private void Confirm()
+        {
             // TODO: SHOULD VALIDATE IF THE PATH IS VALID.
             this.ViewModel.SelectedFolder = new Domain.Folder { Path = selectedPathTextBox.Text };
             this.ViewModel.HasConfirmed = true;
             this.Close();
         }
 
-        private void Cancel_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        private void Cancel()
         {
             this.ViewModel.HasConfirmed = false;
             this.Close();
