e88cc2ee9afa1ee12876fea4873cf29b431d6886
Juan Pablo Drexler <jpablodrexler@outlook.com>
2021-11-27 22:04:14 -0300

    Checks new release.

--- JPPhotoManager/JPPhotoManager.Domain/Release.cs
+++ JPPhotoManager/JPPhotoManager.Domain/Release.cs
@@ -4,5 +4,7 @@
     {
         public string Name { get; set; }
         public DateTimeOffset? PublishedAt { get; set; }
+        public bool IsNewRelease { get; set; }
+        public bool Success { get; set; }
     }
 }
