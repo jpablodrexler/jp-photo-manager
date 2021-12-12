f963179d62f4cb313d4fa6d550d2301e63d438e5
Juan Pablo Drexler <jpablodrexler@outlook.com>
2021-12-12 19:41:46 -0300

    Updates test with coverage batch.

--- JPPhotoManager/test-with-coverage.bat
+++ JPPhotoManager/test-with-coverage.bat
@@ -1,5 +1,9 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*Tests*]" /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromCodeCoverageAttribute"
+rmdir /S /Q .\TestResults
+rmdir /S /Q .\JPPhotoManager.Tests\TestResults
 
-dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
-.\tools\reportgenerator.exe -reports:JPPhotoManager.Tests\TestResults\coverage.info -targetdir:.\TestResults\
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromCodeCoverageAttribute" JPPhotoManager.sln --collect:"XPlat Code Coverage"
+
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path Tools
+.\Tools\reportgenerator.exe -reports:JPPhotoManager.Tests\TestResults\*\coverage.cobertura.xml -targetdir:.\TestResults\
+
 start .\TestResults\index.htm
