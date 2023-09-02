ac69df7bf4e6e5a7ea055808ad529d45a5f0bd21
Juan Pablo Drexler <jpablodrexler@outlook.com>
2023-09-01 22:10:38 -0300

    Initialize dbcontext.

--- JPPhotoManager/JPPhotoManager.Infrastructure/AppDbContext.cs
+++ JPPhotoManager/JPPhotoManager.Infrastructure/AppDbContext.cs
@@ -1,10 +1,21 @@
 ﻿using JPPhotoManager.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace JPPhotoManager.Infrastructure
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext() : base()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             
@@ -12,5 +23,16 @@ namespace JPPhotoManager.Infrastructure
 
         public DbSet<Asset> Assets { get; set; }
         public DbSet<Folder> Folders { get; set; }
+
+        // Overriding this method is needed to execute the EF commands.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlite(configuration.GetConnectionString("SqliteConnection"));
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
