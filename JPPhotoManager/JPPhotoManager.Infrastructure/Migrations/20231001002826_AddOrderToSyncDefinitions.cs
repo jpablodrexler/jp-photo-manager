99a3515d6ebd31dcc05d6cce35c4feb0dbcb4049
Juan Pablo Drexler <jpablodrexler@outlook.com>
2023-09-30 21:45:56 -0300

    Add migration.

---
+++ JPPhotoManager/JPPhotoManager.Infrastructure/Migrations/20231001002826_AddOrderToSyncDefinitions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JPPhotoManager.Infrastructure.Migrations
+{
+    public partial class AddOrderToSyncDefinitions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "SyncAssetsDirectoriesDefinitions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "SyncAssetsDirectoriesDefinitions");
+        }
+    }
+}
