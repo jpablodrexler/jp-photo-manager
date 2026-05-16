68a47cb941916a3957043335852f04b3d4a913b2
Juan Pablo <jpablodrexler@gmail.com>
2026-05-16 19:35:14 -0300

    refactor: Eliminar excepciones no utilizadas en la gestión de errores

--- JPPhotoManagerWeb/backend/src/main/java/com/jpablodrexler/photomanager/api/exception/AlbumNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jpablodrexler.photomanager.api.exception;
-
-public class AlbumNotFoundException extends RuntimeException {
-    public AlbumNotFoundException(Long albumId) {
-        super("Album not found: " + albumId);
-    }
-}
--- JPPhotoManagerWeb/backend/src/main/java/com/jpablodrexler/photomanager/api/exception/InvalidRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.jpablodrexler.photomanager.api.exception;
-
-public class InvalidRefreshTokenException extends RuntimeException {
-    public InvalidRefreshTokenException() {
-        super("Refresh token is invalid, expired, or revoked");
-    }
-}
